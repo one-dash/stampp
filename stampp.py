ce7301d8152b5478d3170f38b24e18ca66e1bb93
Here I. Come <me.detected@gmail.com>
2011-01-05 05:05:30 +0300

    text extraction function(from status.net API XML response)

--- stampp.py
+++ stampp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from xml.dom import minidom
 import os
 import urllib2
 
@@ -45,8 +46,27 @@ class Connector:
     # TODO: expect things like url cannot be opened
     return urllib2.urlopen(urllib2.Request(self.uri)).read()
 
+def extractText(source):
+  """
+  returns status text from status.net XML response
+
+  source should be well-formed XML status.net response
+  """
+  # TODO: handle parse errors
+  wholexml = minidom.parseString(source)
+  # TODO: handle absense of XML tags
+  statuses = wholexml.getElementsByTagName('statuses')[0]
+  # TODO: handle absense of any text entries in user's microblog
+  laststatus = statuses.getElementsByTagName('status')[0]
+  textelemnt = laststatus.getElementsByTagName('text')[0]
+  text = textelemnt.firstChild.data
+
+  return text
+
 if __name__ == "__main__":
   config = Config(os.path.expanduser("~/.stampp"))
   uri = "https://identi.ca/api/statuses/user_timeline/" +\
       config.getUsername() + ".xml?count=1"
   connector = Connector(uri)
+
+  extractText(connector.getData())
